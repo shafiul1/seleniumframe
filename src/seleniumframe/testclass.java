23b8daf5335262443f7c5debf3550058a22394e3
Shafiul Islam <shafiul.is@gmail.com>
2018-09-30 11:41:33 -0400

    test

---
+++ src/seleniumframe/testclass.java
@@ -0,0 +1,14 @@
+package seleniumframe;
+
+public class testclass {
+
+	public static void main(String[] args) {
+		
+		String a="whats up goog people";
+		
+				System.out.println(a);
+		
+		
+	}
+
+}
