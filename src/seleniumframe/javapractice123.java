3b7a3e60591ce7fbf634c1831e3456c6e6603602
Shafiul Islam <shafiul.is@gmail.com>
2018-10-07 12:36:41 -0400

    selenium added
    selenium added

---
+++ src/seleniumframe/javapractice123.java
@@ -0,0 +1,33 @@
+package seleniumframe;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class javapractice123 {
+
+	public static void main(String[] args) {
+		
+		
+//		String ab="okay i gt you";
+//		String[] ac=ab.split(" ");
+//		System.out.println(ac[1]+" "+ac[3]);
+//		System.out.println(ab.replaceAll(" ", ""));
+		
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
--- src/seleniumframe/testclass.java
+++ src/seleniumframe/testclass.java
@@ -1,12 +1,16 @@
 package seleniumframe;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class testclass {
 
 	public static void main(String[] args) {
 		
 		String a="whats up goog people";
 		
-				System.out.println(a);
+		WebDriver driver= new FirefoxDriver();
+		driver.get("https://www.facebook.com/");
 				
 		// my third line
 		
